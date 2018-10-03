af0748acd78a4cd97c80060e9d9bf0081fa6f8e6
Renan Santana <renan_ota@hotmail.com>
2018-10-02 23:22:48 -0300

    Finalizado

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from tkinter import filedialog
 class App:
 
     def __init__(self, root):
-        
+
         root.title("Multi alterador.")
         root.resizable(0,0)
 
+        # Frame raíz
         self.frameMain = Frame(root)
         self.frameMain.pack()
 
@@ -31,31 +32,51 @@ class App:
         self.areaText = Text(self.frameText, height=10, width=60)
         self.areaText.pack()
 
+        # Radios
+        self.posicao = IntVar()
+        self.frameRadios = Frame(self.frameMain)
+        self.frameRadios.pack(fill='x')
+        self.radioA = Radiobutton(self.frameRadios, text="Adicionar ao início", variable=self.posicao, value=1)
+        self.radioA.pack(side=LEFT)
+        self.radioA.select()
+        self.radioB = Radiobutton(self.frameRadios, text="Adicionar ao final", variable=self.posicao, value=2)
+        self.radioB.pack(side=LEFT)
+
+        # Play
+        self.btPlay = Button(self.frameText, text="Gravar", command=self.modifyFiles)
+        self.btPlay.pack(fill='x')
+
     def setPath(self):
         """ Pergunta ao usuário a pasta que será modificada e salva o caminho na variável 'path'. """
         self.path = filedialog.askdirectory(title="Selecione a pasta onde os arquivos estão")
+        self.inputPath.delete(0, END)
         self.inputPath.insert(0, self.path)
+        self.setFilesName()
 
     def setFilesName(self):
         """ Cria uma lista com os arquivos que estão na pasta na pasta. """
         self.files = os.listdir(self.path)
 
-    def modifuFiles(self):
+    def getText(self):
+        """ Pega o texto digitado no areaText e armazena em uma variável. """
+        self.text = self.areaText.get("0.0", END)
+
+    def clearText(self):
+        """ Limpa o conteúdo do areaText """
+        self.areaText.delete("0.0", END)
+
+    def modifyFiles(self):
         """ Modifica todos os arquivos em self.files e adiciona o que está no self.text """
-        pass
-
-    """
-    for file in files:
-
-        fileOpen = open(file, "r+")
-        content = arquivo.readlines()
-        content.insert(0, texto)
-
-        fileOpen.seek(0,0)
-        fileOpen.writelines(content)
-        fileOpen.close()
-    """
-
+        self.getText()
+        for file in self.files:
+            fileOpen = open(self.path + '/' + file, "r+")
+            content = fileOpen.readlines()
+            posicao = 0 if self.posicao == 1 else len(content)
+            content.insert(posicao, self.text)
+            fileOpen.seek(0,0)
+            fileOpen.writelines(content)
+            fileOpen.close()
+        self.clearText()
 
 if __name__ == '__main__':
     root = Tk()
